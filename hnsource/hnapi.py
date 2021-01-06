ad7d9086fd746e2c001d6b286c8b80a761a6fcfe
Pedro Lopes <3640622+lopespm@users.noreply.github.com>
2021-01-06 02:19:50 +0000

    Skip comments which do not have text

--- hnsource/hnapi.py
+++ hnsource/hnapi.py
@@ -14,16 +14,28 @@ def request_hn_best_stories():
 def get_comment(comment_id):
 	logging.debug(f'Getting comment for comment_id {comment_id}')
 	full_comment = request_hn_id_details(comment_id)
+	if ('text' not in full_comment):
+		return None
 	soup = BeautifulSoup(full_comment['text'], 'html.parser')
 	comment_text = soup.text
 	return Comment(id=comment_id, text=comment_text)
+
+def get_comments(comment_ids, number_of_comments):
+	for comment_id in comment_ids:
+		if (number_of_comments <= 0):
+			break
+		comment = get_comment(comment_id)
+		if (comment is not None):
+			number_of_comments -= 1
+			yield comment
 
 def top_comments(hn_story_id, number_of_comments=3):
 	logging.info(f'Getting comments for story {hn_story_id}')
 	hn_story = request_hn_id_details(hn_story_id)
 	if ('kids' in hn_story):
-		return [get_comment(comment_id) for comment_id in hn_story['kids'][:number_of_comments]]
+		return [comment for comment in get_comments(hn_story['kids'], number_of_comments)]
 	return []
+
 
 def best_story_ids():
 	logging.info(f'Fetching best stories from API')
